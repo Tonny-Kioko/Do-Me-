b86711753e4838cf2272209869385799269e41e7
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-12 14:50:39 +0300

    setting up views and the URLs

--- do_me_project/base/urls.py
+++ do_me_project/base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import taskList
+from .views import taskDetail
 
 urlpatterns = [
     path('', taskList.as_view(), name='tasks'), 
+    path('task/<int:pk>/', taskDetail.as_view(), name='tasks_detail'),
 ]
--- do_me_project/base/views.py
+++ do_me_project/base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
 class taskList(ListView):
     model = Task
+    context_object_name = 'tasks'
+
+
+class taskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
