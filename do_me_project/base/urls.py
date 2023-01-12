69dd502bef33c8f8ad0b768246ee10cecaf85a8c
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-12 16:14:40 +0300

    building CRUD functionality

--- do_me_project/base/urls.py
+++ do_me_project/base/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import taskList
+from .views import taskDelete, taskList, taskUpdate
 from .views import taskDetail
 from .views import taskCreate
 
 urlpatterns = [
     path('', taskList.as_view(), name='tasks'), 
+
+    ##CRUD Functionality
     path('task/<int:pk>/', taskDetail.as_view(), name='task'),
     path('create-task/', taskCreate.as_view(), name='task-create'),
+    path('update-task/<int:pk>/', taskUpdate.as_view(), name='task-update'),
+    path('delete-task/<int:pk>/', taskDelete.as_view(), name='task-delete'),
+
+    
 ]
--- do_me_project/base/views.py
+++ do_me_project/base/views.py
@@ -3,9 +3,13 @@ from django.views.generic.list import ListView
 from .models import Task
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
 
 # Create your views here.
+
+##CRUD Classes
 
 class taskList(ListView):
     model = Task
@@ -22,3 +26,14 @@ class taskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+
+class taskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class taskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task' 
+    success_url = reverse_lazy('tasks')   
