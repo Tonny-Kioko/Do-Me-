a4951b2f66edaf1829b08cc4441d7f056c087fa2
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-13 12:16:11 +0300

    setting up deployment

--- do_me_project/do_me_project/settings.py
+++ do_me_project/do_me_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xp+7x24@36yvqy)tsg10vdoavldz9!0)x1hfea-2ll$@0m0edq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://do-me-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
