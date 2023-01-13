9ad6152f4c72c2fa920e443cd223276a2f6776a0
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-13 12:42:17 +0300

    deploying

--- do_me_project/do_me_project/settings.py
+++ do_me_project/do_me_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
