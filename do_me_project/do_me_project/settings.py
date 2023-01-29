480a42744ab2bc8d24d006f6764d48846c125364
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-29 15:20:55 +0300

    API testing

--- do_me_project/do_me_project/settings.py
+++ do_me_project/do_me_project/settings.py
@@ -121,14 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
+# STATIC_URL = 'static/'
+# MEDIA_URL = '/images/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# MEDIA_ROOT = os.path.join(BASE_DIR, '')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
